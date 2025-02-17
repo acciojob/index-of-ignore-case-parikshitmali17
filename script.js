c9abc6a0f71be6267e7ed61c4066be82ab2e9d62
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-17 18:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let boole=0;
+	for (let index = 0; index < s2.length; index++) {
+		for (let index1 = 0; index1 < s1.length; index1++) {
+			if(s1[index]==s2[index]){
+				return index
+			}else{
+				boole=-1
+			}
+		}
+	}
+
+	return (index) ? index : boole
+	
 }
 
 // Please do not change the code below
